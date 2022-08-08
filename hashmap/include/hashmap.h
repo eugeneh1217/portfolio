e0959260383056ad29c60d8199cfc7333838d28f
Eugene Hong <eugeneh1217@gmail.com>
2022-08-08 12:42:09 -0400

    Start bucket_t

--- hashmap/include/hashmap.h
+++ hashmap/include/hashmap.h
@@ -1,17 +1,52 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
+#include <stdlib.h>
 #include <string.h>
 
+struct pair_t;
+struct bucket_t;
 struct hashmap_t;
+
+typedef struct pair_t
+{
+    size_t first_size;
+    size_t second_size;
+    void *first;
+    void *second;
+} pair_t;
+
+typedef struct bucket_t
+{
+    size_t size;
+    size_t count;
+} bucket_t;
 
 typedef struct hashmap_t
 {
-
+    size_t size_;
+    size_t count;
 } hashmap_t;
 
-hashmap_t *init_hashmap();
+// pair memory management
+pair_t *init_pair(size_t first_size, size_t second_size);
+pair_t *init_pair_values(size_t first_size, size_t second_size, void *first_value, void *second_value);
+void free_pair(pair_t *pair);
 
+// pair data management
+void pair_insert_first(pair_t *pair, const void *value);
+void pair_insert_second(pair_t *pair, const void *value);
+void pair_insert(pair_t *pair, const void *first_value, const void *second_value);
+
+// bucket memory management
+bucket_t *init_bucket();
+void free_bucket(bucket_t *bucket);
+
+// bucket data management
+void bucket_insert(bucket_t *bucket, const pair_t *pair);
+
+// hasmap memory management
+hashmap_t *init_hashmap();
 void free_hashmap(hashmap_t *hashmap);
 
 #endif
--- hashmap/test/src/test_hashmap.cc
+++ hashmap/test/src/test_hashmap.cc
@@ -6,7 +6,7 @@ extern "C"
     #include "hashmap.h"
 }
 
-class HashMapTest : public ::testing::Test
+class HashmapTest : public ::testing::Test
 {
     public:
         hashmap_t *hashmap;
@@ -22,3 +22,104 @@ class HashMapTest : public ::testing::Test
         }
 };
 
+TEST_F(HashmapTest, TestInit)
+{
+    EXPECT_EQ(hashmap->size_, 1);
+    EXPECT_EQ(hashmap->count, 0);
+}
+
+class PairTest : public ::testing::Test
+{
+    public:
+        pair_t *pair;
+
+        void SetUp()
+        {
+            pair = init_pair(sizeof(char), sizeof(int));
+        }
+
+        void TearDown()
+        {
+            free_pair(pair);
+        }
+};
+
+TEST_F(PairTest, TestInit)
+{
+    pair->first;
+    pair->second;
+    EXPECT_EQ(pair->first_size, sizeof(char));
+    EXPECT_EQ(pair->second_size, sizeof(int));
+}
+
+TEST_F(PairTest, TestValueConstructor)
+{
+    char first = 'h';
+    int second = 42;
+    pair_t *value_pair = init_pair_values(
+        sizeof(first),
+        sizeof(second),
+        (void *) &first,
+        (void *) &second
+    );
+    free_pair(value_pair);
+}
+
+TEST_F(PairTest, TestInsertFirst)
+{
+    char first_value = 'h';
+    pair_insert_first(pair, (void *) &first_value);
+    EXPECT_EQ(*(char *) pair->first, 'h');
+}
+
+TEST_F(PairTest, TestInsertSecond)
+{
+    int second_value = 42;
+    pair_insert_second(pair, (void *) &second_value);
+    EXPECT_EQ(*(int *) pair->second, 42);
+}
+
+TEST_F(PairTest, TestInsert)
+{
+    char first_value = 'h';
+    int second_value = 42;
+    pair_insert(pair, (void *) &first_value, (void *) &second_value);
+    EXPECT_EQ(*(char *) pair->first, 'h');
+    EXPECT_EQ(* (int *) pair->second, 42);
+}
+
+class BucketTest : public ::testing::Test
+{
+    public:
+        bucket_t *bucket;
+
+        void SetUp()
+        {
+            bucket = init_bucket();
+        }
+
+        void TearDown()
+        {
+            free_bucket(bucket);
+        }
+};
+
+TEST_F(BucketTest, TestInit)
+{
+    EXPECT_EQ(bucket->size, 1);
+    EXPECT_EQ(bucket->count, 0);
+}
+
+TEST_F(BucketTest, TestInsert)
+{
+    char key0 = 'a';
+    int value0 = 2;
+    pair_t *pair0 = init_pair_values(
+        sizeof(char),
+        sizeof(int),
+        (void *) &key0,
+        (void *) &value0
+    );
+    bucket_insert(bucket, pair0);
+    free_pair(pair0);
+}
