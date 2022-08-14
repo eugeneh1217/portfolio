e85dfd72f987df2eca0f51a3290b90716e73b7d2
Eugene Hong <eugeneh1217@gmail.com>
2022-08-14 17:02:39 -0400

    Draft basic insertion

--- hashmap/include/hashmap.h
+++ hashmap/include/hashmap.h
@@ -6,4 +6,36 @@
 
 // TODO: add github workflow for todo and test passing
 
+typedef enum STATUS_T
+{
+    SUCCESS=0,
+    KEY_NOT_FOUND=1
+} STATUS_T;
+
+typedef struct item_t item_t;
+
+typedef struct item_t
+{
+    item_t *prev;
+    item_t *next;
+    void *k;
+    void *v;
+} item_t;
+
+typedef struct hashmap_t
+{
+    size_t k_size;
+    size_t v_size;
+    size_t count;
+    item_t *items;
+} hashmap_t;
+
+hashmap_t *init_hashmap(size_t, size_t);
+void free_hashmap(hashmap_t *);
+
+void hashmap_insert(hashmap_t *, const void *, const void *);
+STATUS_T hashmap_get(hashmap_t *, const void *, void *);
+void hashmap_delete(hashmap_t *, const void *);
+
+
 #endif
