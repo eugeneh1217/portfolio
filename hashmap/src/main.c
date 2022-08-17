70accd8bfaf9a7984f58a96308570acdef99935e
Eugene Hong <eugeneh1217@gmail.com>
2022-08-17 10:44:12 -0400

    Add access and deletion demos

--- hashmap/src/main.c
+++ hashmap/src/main.c
@@ -14,13 +14,16 @@ size_t hash_int(const void *key, size_t size)
 int main()
 {
     system("clear");
-    printf("HASHMAP DEMO\n");
+    printf("HASHMAP DEMO\n\n");
 
     hashmap_t *map = init_hashmap(KEY_SIZE, VALUE_SIZE, hash_int);
 
     clock_t time;
     double elapsed;
+    int ret;
+
     int element_count = 10000;
+    printf("Element Insertion:\n");
     for (int i = 0; i < 10; ++ i)
     {
         time = clock();
@@ -37,6 +40,53 @@ int main()
         element_count *= 2;
         hashmap_clear(map);
     }
+
+    element_count = 10000;
+    printf("Element Access:\n");
+    for (int i = 0; i < 10; ++ i)
+    {
+        time = clock();
+        for (int i = 0; i < element_count; ++ i)
+        {
+            hashmap_insert(map, &i, &i);
+        }
+        for (int i = 0; i < element_count; ++ i)
+        {
+            hashmap_get(map, &i, &ret);
+        }
+
+        time = clock() - time;
+        elapsed = (double) time / CLOCKS_PER_SEC;
+
+        printf("\tTime to access %d elements: %f, time / element: %lf\n",
+               element_count, elapsed, elapsed / element_count);
+        element_count *= 2;
+        hashmap_clear(map);
+    }
+
+    element_count = 10000;
+    printf("Element Deletion:\n");
+    for (int i = 0; i < 10; ++ i)
+    {
+        time = clock();
+        for (int i = 0; i < element_count; ++ i)
+        {
+            hashmap_insert(map, &i, &i);
+        }
+        for (int i = 0; i < element_count; ++ i)
+        {
+            hashmap_delete(map, &i);
+        }
+
+        time = clock() - time;
+        elapsed = (double) time / CLOCKS_PER_SEC;
+
+        printf("\tTime to delete %d elements: %f, time / element: %lf\n",
+               element_count, elapsed, elapsed / element_count);
+        element_count *= 2;
+        hashmap_clear(map);
+    }
+
     printf("DONE\n");
 
     free_hashmap(map);
