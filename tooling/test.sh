a8a01cf2f660a52518559852c0de5192f1748c89
Eugene Hong <eugeneh1217@gmail.com>
2022-08-19 13:27:12 -0400

    Add legibility changes to test script

--- tooling/test.sh
+++ tooling/test.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
+
 set -e
+
 for project in *; do
+
     if [ -d "$project" ]; then
+
         printf "testing \"%s\" project...\n" $project
-        if [ -d "$project/src" ]; then
-            printf "language: c\n"
+
+        if [ -d "$project/src" ]; then # project identified as c/c++
+
+            printf "language: c/c++\n"
+
             if [ -d "$project/test" ]; then
+
+                # build project
                 cd $project
                 cmake -B build
                 cd build
                 make
+
+                # run tests
                 valgrind ./tests
+
+                # back to root directory
                 cd ../..
+
                 printf "\n"
             fi
-        elif [ -f "$project/__init__.py" ]; then
+        elif [ -f "$project/__init__.py" ]; then # project identified as python
+
             printf "language: python\n"
+
             if [ -d "$project/test" ]; then
+
                 pip install -r $project/requirements.txt
+
                 python3 -m unittest discover $project
+
                 printf "\n"
+
             else
                 printf "no tests found\n\n"
             fi
@@ -28,4 +48,5 @@ for project in *; do
         fi
     fi
 done
+
 echo -e  "\e[1;32mAll Tests Passed\e[0m"
